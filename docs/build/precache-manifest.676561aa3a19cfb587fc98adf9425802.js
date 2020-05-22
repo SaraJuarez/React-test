6f239b164e19af7e669d3bb3107f810d1921047f
adalaber <sarajuarezzarco@gmail.com>
2020-05-22 16:12:57 +0200

    Add build app

---
+++ docs/build/precache-manifest.676561aa3a19cfb587fc98adf9425802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51afba67a2ca70ccc409027f9c0ef1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d9243c97bd3f2553ddd",
+    "url": "./static/css/main.306706c5.chunk.css"
+  },
+  {
+    "revision": "b33b5065cd4688721edf",
+    "url": "./static/js/2.2f70bbfc.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "./static/js/2.2f70bbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9243c97bd3f2553ddd",
+    "url": "./static/js/main.34502bb3.chunk.js"
+  },
+  {
+    "revision": "d4cdc0c52f6c8c0611be",
+    "url": "./static/js/runtime-main.622b39b6.js"
+  }
+]);
